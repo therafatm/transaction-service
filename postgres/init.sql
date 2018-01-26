49216c7c3a293929d3d401e8fdf89ca924d9b79f
Cameron Long <cdalong@uvic.ca>
2018-01-25 18:20:44 -0800

    moved postgres up one level

--- postgres/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER develop with password 'dev';
-CREATE DATABASE liefbase;
-CREATE extension postgis;
-GRANT ALL PRIVILEGES ON DATABASE liefbase TO develop;
-alter user develop with superuser;
